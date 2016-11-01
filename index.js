aba5da0f0970275eb1e268a428e8943a93cba2f0
Bruzual Balzan Daniel <bruzuad1@niksula.hut.fi>
2016-11-01 23:44:17 +0200

    Recognise audio with Google Speech

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import express from 'express'
-import multer from 'multer'
-import bodyParser from 'body-parser'
-// import fetch from 'node-fetch'
-import speech from 'google-speech-api'
+var express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var request = require('request');
+var fs = require('fs');
+
+
+var speech = require('@google-cloud/speech')();
+var ffmpeg = require('fluent-ffmpeg');
+var uuid = require('node-uuid');
+
+//where to save telegram voice notes
+var tmpDir = './audios/';
 
 const opts = {
   key: process.env.SPEECH_API_KEY
@@ -19,12 +27,69 @@ app
   res.send('OK')
 })
 .post('/', (req, res) => {
-  request
-  .get(req.body.audioFileUrl)
-  .pipe(speech(opts, (err, res) => {
-    console.log(err)
-    console.log(res)
-    res.send('OOK')
-  }))
-})
+  //download voicenote to disk with unique name
+  var output = tmpDir + uuid.v4() + ".flac";
+  ffmpeg(request(req.body.audioFileUrl))
+    .output(output)
+    .outputOptions(['-ac 1', '-ar 16000']) //1 channel, sampleRate 16.000
+    .on('error', function(err) {
+      console.log('An error occurred: ' + err.message);
+    })
+    .on('end', function() {
+      //file downloaded, send to Google Speech
+      syncRecognize(output, function(results){
+        //results is the string containing the interpreted audio
+        console.log(results);
+
+        //TODO: hamming distance to find closest match?
+
+        //TODO: respond with closest match
+        
+        //delete file
+        fs.unlink(output);
+      });
+    })
+    .run();
+
+  })
 .listen(process.env.PORT || 5000)
+
+
+/*
+ * Taken from https://github.com/GoogleCloudPlatform/nodejs-docs-samples
+ */
+function asyncRecognize (filename, callback) {
+  // Detect speech in the audio file, e.g. "./resources/audio.raw"
+  speech.startRecognition(filename, {
+    encoding: 'FLAC',
+    sampleRate: 16000,
+    languageCode: 'fi-FI'
+  }, (err, operation) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    operation
+      .on('error', callback)
+      .on('complete', (results) => {
+        callback(results);
+      });
+  });
+}
+
+function syncRecognize (filename, callback) {
+  // Detect speech in the audio file, e.g. "./resources/audio.raw"
+  speech.recognize(filename, {
+    encoding: 'FLAC',
+    sampleRate: 16000,
+    languageCode: 'fi-FI'
+  }, (err, results) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    callback(results);
+  });
+}
